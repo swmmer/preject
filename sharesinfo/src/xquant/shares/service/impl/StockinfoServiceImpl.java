59a629705eabe1e2b1b3142db63d364a0d0a11b4
tangruikun <936761646@qq.com>
2018-03-14 12:35:54 +0800

    根据前台返回查询字段导出excel

--- sharesinfo/src/xquant/shares/service/impl/StockinfoServiceImpl.java
+++ sharesinfo/src/xquant/shares/service/impl/StockinfoServiceImpl.java
@@ -1,6 +1,17 @@
 package xquant.shares.service.impl;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +20,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import xquant.shares.dao.stockinfoDao;
+import xquant.shares.model.ExportExcel;
 import xquant.shares.model.stockinfo;
 import xquant.shares.service.StockinfoService;
 
@@ -50,5 +62,34 @@ public class StockinfoServiceImpl implements StockinfoService{
 		return stockOfJson;	
 		
 	}
+	
+	@Override
+	public void exportExcel(String stockCodeSearch){
+		
+		List<stockinfo> stockInfoList = stockinfodao.selectBystockCode(stockCodeSearch);
+		
+		 String path = "E://NewFileName.xls";
+		 String[] headers =  
+	          { "股票代码", "股票名称", "交易市场", "发行价格(元/股)", "发行市盈率","上市日期","到期日" };  
+	     
+	     ExportExcel<stockinfo> ex = new ExportExcel<stockinfo>(); 
+	     OutputStream out;
+		try {
+			out = new FileOutputStream(path);
+			 ex.exportExcel(headers,stockInfoList, out);  
+		      
+		      try {
+				out.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	      
+	}
+	
 	
 }
