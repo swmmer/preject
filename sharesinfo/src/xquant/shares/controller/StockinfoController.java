50b15a9579db663bf01473284f90847351740e12
DESKTOP-RONQQOH\swmmer <906003904@qq.com>
2018-03-14 14:05:13 +0800

    冲突

--- sharesinfo/src/xquant/shares/controller/StockinfoController.java
+++ sharesinfo/src/xquant/shares/controller/StockinfoController.java
@@ -1,6 +1,5 @@
 package xquant.shares.controller;
 
-<<<<<<< HEAD
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -8,9 +7,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-=======
-import java.text.ParseException;
->>>>>>> 55ddf1fa49620854899e71bad6be04bc878e62d2
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -65,12 +61,42 @@ public class StockinfoController {
 	}
     
 	@RequestMapping("exportExcelByStockCodeSearch")
-	public void exportExcelByStockCodeSearch(String stockCodeSearch,HttpServletResponse response,HttpServletRequest request){
+	public void exportExcelByStockCodeSearch(String stockCodeSearch,HttpServletResponse response){
 		
 		stockinfoService.exportExcel(stockCodeSearch);
 		String path = "E://NewFileName.xls";
-		Result.download(path,response);
+		download(path,response);
 	}
 	
-	
+	private void download(String path, HttpServletResponse response) {  
+        try {  
+            // path是指欲下载的文件的路径。  
+            File file = new File(path);  
+            // 取得文件名。  
+            String filename = file.getName();  
+            // 以流的形式下载文件。  
+            InputStream fis = new BufferedInputStream(new FileInputStream(path));  
+            byte[] buffer = new byte[fis.available()];
+            
+            fis.read(buffer);  
+            fis.close();  
+            
+            // 清空response  
+            response.reset();  
+            // 设置response的Header  
+            response.addHeader("Content-Disposition", "attachment;filename="  
+                    + new String(filename.getBytes()));  
+            response.addHeader("Content-Length", "" + file.length());  
+            OutputStream toClient = new BufferedOutputStream(  
+                    response.getOutputStream());  
+            response.setContentType("application/vnd.ms-excel;charset=gb2312");  
+            toClient.write(buffer);  
+            toClient.flush();
+            toClient.close(); 
+            file.delete();
+            
+        } catch (IOException ex) {  
+            ex.printStackTrace();  
+        }  
+    }  
 }
