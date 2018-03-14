04897e5d2a4ad194bc406f80046a131d223c78d7
DESKTOP-RONQQOH\swmmer <906003904@qq.com>
2018-03-14 11:38:44 +0800

    登陆接口

--- sharesinfo/src/xquant/shares/service/UserService.java
+++ sharesinfo/src/xquant/shares/service/UserService.java
@@ -1,13 +1,8 @@
 package xquant.shares.service;
 
-import java.util.List;  
 import xquant.shares.model.User;  
   
 public interface UserService {  
-    String addInfo(User addInfo);  
-    List<User> getAll();  
-    String delete(int id);  
-    User findById(int id);  
-    String update(User addInfo);  
+   
     User login(User user);  
 }  
